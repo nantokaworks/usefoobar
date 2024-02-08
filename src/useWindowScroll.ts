f58c0d0ea9680b11b76a25fb805525b36b0ba56a
Yamashita, Shinichi <ichi0g0y@gmail.com>
2024-02-08 13:50:33 +0900

    :+1: add scrolling flag to useWindowScroll

--- src/useWindowScroll.ts
+++ src/useWindowScroll.ts
@@ -1,6 +1,7 @@
 import { useCallback, useLayoutEffect, useState } from 'react'
 
 export function useWindowScroll() {
+  const [scrolling, setScrolling] = useState(false)
   const [scroll, setScroll] = useState<{ x: number | undefined; y: number | undefined }>({
     x: undefined,
     y: undefined,
@@ -13,15 +14,21 @@ export function useWindowScroll() {
   useLayoutEffect(() => {
     const handleScroll = () => {
       setScroll({ x: window.scrollX, y: window.scrollY })
+      setScrolling(true)
+    }
+    const handleScrollEnd = () => {
+      setScrolling(false)
     }
 
     handleScroll()
     window.addEventListener('scroll', handleScroll)
+    window.addEventListener('scrollend', handleScrollEnd)
 
     return () => {
       window.removeEventListener('scroll', handleScroll)
+      window.removeEventListener('scrollend', handleScrollEnd)
     }
   }, [])
 
-  return [scroll, scrollTo]
+  return { scrolling, scroll, scrollTo }
 }
